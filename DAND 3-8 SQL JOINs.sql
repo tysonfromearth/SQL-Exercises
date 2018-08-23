fc76b0c8ed9b022e8a8920c517fb33ed6564224a
Tyson Bryant <t.l.bryant@aggiemail.usu.edu>
2018-08-23 09:50:11 -0700

    feat: Add answers for "Last Check" in 'DAND 3-8 SQL JOINs.sql'

--- DAND 3-8 SQL JOINs.sql
+++ DAND 3-8 SQL JOINs.sql
@@ -49,6 +49,14 @@ associated accounts. This time only for the Midwest region. Your final table
 should include three columns: the region name, the sales rep name, and the
 account name. Sort the accounts alphabetically (A-Z) according to account name.
 */
+SELECT r.name region, sr.name rep, a.name account
+  FROM accounts a
+  JOIN sales_reps sr
+  ON a.sales_rep_id = sr.id
+  JOIN region r
+  ON sr.region_id = r.id
+  WHERE r.name = 'Midwest'
+  ORDER BY a.name;
 
 /*
 2. Provide a table that provides the region for each sales_rep along with their
@@ -57,6 +65,14 @@ name starting with S and in the Midwest region. Your final table should include
 three columns: the region name, the sales rep name, and the account name. Sort
 the accounts alphabetically (A-Z) according to account name.
 */
+SELECT r.name region, sr.name rep, a.name account
+  FROM accounts a
+  JOIN sales_reps sr
+  ON a.sales_rep_id = sr.id
+  JOIN region r
+  ON sr.region_id = r.id
+  WHERE r.name = 'Midwest' AND sr.name LIKE 'S%'
+  ORDER BY a.name;
 
 /*
 3. Provide a table that provides the region for each sales_rep along with their
@@ -65,6 +81,14 @@ name starting with K and in the Midwest region. Your final table should include
 three columns: the region name, the sales rep name, and the account name. Sort
 the accounts alphabetically (A-Z) according to account name.
 */
+SELECT r.name region, sr.name rep, a.name account
+  FROM accounts a
+  JOIN sales_reps sr
+  ON a.sales_rep_id = sr.id
+  JOIN region r
+  ON sr.region_id = r.id
+  WHERE r.name = 'Midwest' AND sr.name LIKE '% K%'
+  ORDER BY a.name;
 
 /*
 4. Provide the name for each region for every order, as well as the account name
@@ -74,6 +98,16 @@ final table should have 3 columns: region name, account name, and unit price. In
 order to avoid a division by zero error, adding .01 to the denominator here is
 helpful total_amt_usd/(total+0.01).
 */
+SELECT r.name region, a.name account, o.total_amt_usd/(o.total + .01) unit_price
+  FROM orders o
+  JOIN accounts a
+  ON a.id = o.account_id
+  JOIN sales_reps sr
+  ON a.sales_rep_id = sr.id
+  JOIN region r
+  ON sr.region_id = r.id
+  WHERE o.standard_qty > 100
+  ORDER BY a.name;
 
 /*
 5. Provide the name for each region for every order, as well as the account name
@@ -84,6 +118,16 @@ region name, account name, and unit price. Sort for the smallest unit price
 first. In order to avoid a division by zero error, adding .01 to the denominator
 here is helpful (total_amt_usd/(total+0.01).
 */
+SELECT r.name region, a.name account, o.total_amt_usd/(o.total + .01) unit_price
+  FROM orders o
+  JOIN accounts a
+  ON a.id = o.account_id
+  JOIN sales_reps sr
+  ON a.sales_rep_id = sr.id
+  JOIN region r
+  ON sr.region_id = r.id
+  WHERE o.standard_qty > 100 AND o.poster_qty > 50
+  ORDER BY a.name;
 
 /*
 6. Provide the name for each region for every order, as well as the account name
@@ -92,16 +136,38 @@ should only provide the results if the standard order quantity exceeds 100 and
 he poster order quantity exceeds 50. Your final table should have 3 columns:
 region name, account name, and unit price. Sort for the largest unit price
 first. In order to avoid a division by zero error, adding .01 to the
-denominator here is helpful (total_amt_usd/(total+0.01). 
+denominator here is helpful (total_amt_usd/(total+0.01).
 */
+SELECT r.name region, a.name account, o.total_amt_usd/(o.total + .01) unit_price
+  FROM orders o
+  JOIN accounts a
+  ON a.id = o.account_id
+  JOIN sales_reps sr
+  ON a.sales_rep_id = sr.id
+  JOIN region r
+  ON sr.region_id = r.id
+  WHERE o.standard_qty > 100 AND o.poster_qty > 50
+  ORDER BY a.name DESC;
 
 /*
 7. What are the different channels used by account id 1001? Your final table
 should have only 2 columns: account name and the different channels. You can try
 SELECT DISTINCT to narrow down the results to only the unique values.
 */
+SELECT DISTINCT a.name, we.channel
+  FROM accounts a
+  JOIN web_events we
+  ON a.id = we.account_id
+  where a.id = 1001;
 
 /*
 8. Find all the orders that occurred in 2015. Your final table should have 4
 columns: occurred_at, account name, order total, and order total_amt_usd.
 */
+SELECT o.occurred_at, a.name, o.total, o.total_amt_usd
+  FROM accounts a
+  JOIN orders o
+  ON o.account_id = a.id
+  WHERE o.occurred_at BETWEEN '2015-01-01'
+        AND '2016-01-01'
+  ORDER BY o.occurred_at DESC;
